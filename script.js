716af9d1415705a4848c6d00e80716489b0ab9fc
emanalarbash <emanm@arbash.co>
2022-08-13 17:20:01 +0300

    done with boooonceeeeee

---
+++ script.js
@@ -0,0 +1,22 @@
+alert("wellcome to my page that will  calculate your score!!")
+let grade = prompt ("right your score with numbers ")
+
+console.log (grade);
+
+if (grade >= 90 ) {
+    console.log (" You got a privilege 🥳")
+}else if (grade >= 80){
+    console.log("you got very good 🤩") 
+} else if (grade >= 70){
+    console.log("you got very good 🙂")
+}else if (grade >=60){
+    console.log("you  got accepted😕")
+}else if (grade >= 50){
+    console.log("you got weak ☹️")
+}else {
+    console.log("you got fail 💔")
+} 
+
+//javascript is not java
+
+console.log('%c Oh my heavens! ', 'background: #222; color: #bada55');
